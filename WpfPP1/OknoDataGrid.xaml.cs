df4f8aca5ccfb0e53ef235e0727ce2c7cfb2f671
Agav <fensagav@gmail.com>
2024-03-16 12:57:01 +0300

    Добавьте файлы проекта.

---
+++ WpfPP1/OknoDataGrid.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfPP1.bazadannihPPDataSetTableAdapters;
+
+namespace WpfPP1 { 
+    public partial class OknoDataGrid : Window
+    {
+        InformationCyberSportaTableAdapter InformationCyberSporta = new InformationCyberSportaTableAdapter();
+        public OknoDataGrid()
+        {
+            InitializeComponent();
+            CybersportDataGrid.ItemsSource = InformationCyberSporta.GetData();
+        }
+
+        private void CybersportDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void Back_knopka_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfPP1/OknoEF.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfPP1.bazadannihPPDataSetTableAdapters;
+
+namespace WpfPP1
+{
+    public partial class OknoEF : Window
+    {
+        private bazadannihPPEntities2 context = new bazadannihPPEntities2();
+        public OknoEF()
+        {
+            InitializeComponent();
+            CybersportDataGrid.ItemsSource = context.InformationCyberSporta.ToList();
+        }
+
+        private void Back_knopka_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Close();
+        }
+    }
+}
